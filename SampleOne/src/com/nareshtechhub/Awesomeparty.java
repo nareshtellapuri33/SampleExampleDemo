a989726318f0594b9e54b3216ecbda845168b7fc
Naresh <nareshtellapuri33@gmail.com>
2023-09-25 21:53:48 +0530

    added one file

---
+++ SampleOne/src/com/nareshtechhub/Awesomeparty.java
@@ -0,0 +1,10 @@
+package com.nareshtechhub;
+
+public class Awesomeparty {
+	public Awesomeparty() {
+		System.out.println("weather is cool");
+	}
+	static {
+		System.out.println("Abc entered");
+	}
+}
