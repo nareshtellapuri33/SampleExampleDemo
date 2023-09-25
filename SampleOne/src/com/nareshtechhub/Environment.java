66a42b2e0300e9527e5088490ff81233b3996ff6
Naresh <nareshtellapuri33@gmail.com>
2023-09-25 21:28:15 +0530

    added new java files

---
+++ SampleOne/src/com/nareshtechhub/Environment.java
@@ -0,0 +1,22 @@
+package com.nareshtechhub;
+
+public class Environment {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Welcome");
+		
+		Environment.Add();
+
+	}
+
+	private static void Add() {
+		int a = 10;
+		int b = 20;
+		
+		int c = a*b;
+		
+		System.out.println(c);
+	}
+
+}
